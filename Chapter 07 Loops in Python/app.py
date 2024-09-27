7018822e062ed2349db0904e4328d6469f5cef3f
bilal-waleed <bilalwaleed246@gmail.com>
2024-09-26 23:57:31 -0700

    Python Learning

---
+++ Chapter 07 Loops in Python/app.py
@@ -0,0 +1,33 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# this is for loop 
+# for i in range(1, 6):
+#     print(i)
+#     i + 1
+
+# this is while loop 
+
+# i = 1 
+
+# while(i < 6):
+#     print(i)
+#     i = i + 1
+
+# l = [1 , "bilal" , "zeenat" , "muuaz" , 5 , 7 ]
+
+# i = 0 
+
+# while(i<len(l)):
+#     print(l[i])
+#     i = i + 1
+
+# for loop in range 
+
+for i in range(0 , 7):
+    print(i)
+
+
