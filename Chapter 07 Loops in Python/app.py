8d384e4c147a6d22fdf48416bdeda533b86acb73
bilal-waleed <bilalwaleed246@gmail.com>
2024-09-27 05:07:28 -0700

    Python Learning

--- Chapter 07 Loops in Python/app.py
+++ Chapter 07 Loops in Python/app.py
@@ -1,29 +1,29 @@
-# print(1)
-# print(2)
-# print(3)
-# print(4)
-# print(5)
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
 
 # this is for loop 
-# for i in range(1, 6):
-#     print(i)
-#     i + 1
+for i in range(1, 6):
+    print(i)
+    i + 1
 
 # this is while loop 
 
-# i = 1 
+i = 1 
 
-# while(i < 6):
-#     print(i)
-#     i = i + 1
+while(i < 6):
+    print(i)
+    i = i + 1
 
-# l = [1 , "bilal" , "zeenat" , "muuaz" , 5 , 7 ]
+l = [1 , "bilal" , "zeenat" , "muuaz" , 5 , 7 ]
 
-# i = 0 
+i = 0 
 
-# while(i<len(l)):
-#     print(l[i])
-#     i = i + 1
+while(i<len(l)):
+    print(l[i])
+    i = i + 1
 
 # for loop in range 
 
@@ -31,3 +31,57 @@ for i in range(0 , 7):
     print(i)
 
 
+# for loop with list 
+
+l = [1 ,4 ,6 ,7 ,8, 9, 3]
+for i in l:
+    print(i)
+
+# for loop with tuples: 
+
+t = (1, 3,334 ,555, 667)
+
+for i in t:
+    print(i)
+
+# for loop in string:
+
+s = "bilal"
+
+for i in s:
+    print(i)
+
+# for loop with else: 
+
+l = [1 ,3 ,4 ,5 ]
+
+for i in l:
+    print(i)
+
+else:
+    print("done") # this is printed when the loop exhausts!
+
+
+# break use in loop
+
+for i in range(100):
+    if(i == 34):
+        break # exit the loop 
+    print(i)
+
+# continue use in loop:
+
+for i in range(100):
+    if(i == 34):
+        continue
+    print(i)
+
+# pass use in loop:
+
+for i in range(100):
+    pass        
+
+i = 0 
+while(i<45):
+    print(i)
+    i = i + 1
