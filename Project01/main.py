610f56e3fd4457a5ffd030337ec87f1a6d692614
bilal-waleed <bilalwaleed246@gmail.com>
2024-09-30 00:42:08 -0700

    Project 01

--- Project01/main.py
+++ Project01/main.py
@@ -6,30 +6,33 @@ import random
 0 for gun
 '''
 
-computer = random.choice([-1,0,1])
-you_str = input("Enter your choice: ")
-you_Dict = {"s" : 1 , "w" : -1 , "g" : 0 }
-reverse_Dict = {1 : "Snake" , -1 : "Water" , 0 : "Gun" }
-you = you_Dict[you_str]
+computer = random.choice([-1, 0, 1])
+you_str = input("Enter your choice (s for Snake, w for Water, g for Gun): ").lower()
 
-print(f"You chose {reverse_Dict[you]}\nComputer chose {reverse_Dict[computer]} ")
+you_Dict = {"s": 1, "w": -1, "g": 0}
+reverse_Dict = {1: "Snake", -1: "Water", 0: "Gun"}
 
-if(computer == you):
-    print("Tie!")
+if you_str not in you_Dict:
+    print("Invalid choice! Please choose either 's', 'w', or 'g'.")
 else:
-    if(computer == -1 and you == 1):
-        print("You Win!")
-    elif(computer == -1 and you == 0):
-        print("You Loss!")
-    elif(computer == 1 and you == -1):
-        print("You Loss!")
-    elif(computer == 1 and you == 0):
-        print("You Win!")
-    elif(computer == 0 and you == -1):
-        print("You Win!")
-    elif(computer == 0 and you == 1):
-        print("You Loss!")
+    you = you_Dict[you_str]
+
+    print(f"You chose {reverse_Dict[you]}\nComputer chose {reverse_Dict[computer]} ")
+
+    if computer == you:
+        print("Tie!")
     else:
-        print("Something went wrong!")
-
-
+        if (computer == -1 and you == 1): 
+            print("You Win!")
+        elif (computer == -1 and you == 0):  
+            print("You Lose!")
+        elif (computer == 1 and you == -1):
+            print("You Lose!")
+        elif (computer == 1 and you == 0):  
+            print("You Win!")
+        elif (computer == 0 and you == -1):
+            print("You Win!")
+        elif (computer == 0 and you == 1):  
+            print("You Lose!")
+        else:
+            print("Something went wrong!")
