c279298e1f56e21c7624411d13c95763b4584130
bilal-waleed <bilalwaleed246@gmail.com>
2024-10-05 13:00:59 +0500

    Python learning

--- Chapter09 File I & O/problem09.py
+++ Chapter09 File I & O/problem09.py
@@ -1,3 +1,12 @@
 # Write a program to find out whether a file is identical & matches the content of another file.
 
-with open("file1.txt", "r")
+with open("this.txt", "r") as f:
+    content1 = f.read()
+
+with open("this_copy.txt", "r") as f:
+    content2 = f.read()
+
+if(content1 == content2):
+    print("Files are identical")
+else:
+    print("Files are not identical")
