52ca49d5d5e0aa6c326157b6ba827a7415bb04ab
bilal-waleed <bilalwaleed246@gmail.com>
2024-10-05 13:26:15 +0500

    Python learning

--- Chapter09 File I & O/problem07.py
+++ Chapter09 File I & O/problem07.py
@@ -1,6 +1,6 @@
 # Write a program to find out the line number where python is present from ques 6.
 
-# this prgram end when the first python in found in the lines 
+# this prgram end when the first python word is found in the lines
 # with open("log.txt" , "r") as f:
 #     lines = f.readlines()
 # lineno = 1
@@ -13,23 +13,7 @@
 # else:
 #     print("python is not present in content")
 
-# these 2 program find the all the words python in all the lines
-
-# with open("log.txt" , "r") as f:
-#     lines = f.readlines()
-# lineno = 1
-# python_lines = []
-# for line in lines:
-#     if "python" in line:
-#         python_lines.append(lineno)  # Store the line number
-#     lineno += 1
-
-# # After all lines are processed, print the results and then break
-# if python_lines:
-#     for line_number in python_lines:
-#         print(f"The word 'python' is present in line no: {line_number}")
-# else:
-#     print("The word 'python' is not present in the content")
+# these 2 program find the all the words python in all the lines of the file
 
 with open("log.txt", "r") as f:
     lines = f.readlines()
@@ -47,3 +31,20 @@ if found:
     print("Finished finding 'python' in all lines.")
 else:
     print("The word 'python' is not present in the content.")
+    
+
+# with open("log.txt" , "r") as f:
+#     lines = f.readlines()
+# lineno = 1
+# python_lines = []
+# for line in lines:
+#     if "python" in line:
+#         python_lines.append(lineno)  # Store the line number
+#     lineno += 1
+
+# # After all lines are processed, print the results and then break
+# if python_lines:
+#     for line_number in python_lines:
+#         print(f"The word 'python' is present in line no: {line_number}")
+# else:
+#     print("The word 'python' is not present in the content")
